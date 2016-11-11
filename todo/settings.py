9b16733175433a2e6f7249155ab6ddaebd01374d
kkriti01 <kriti.cs10@gmail.com>
2016-11-11 15:44:59 +0530

    heroku deployment changes added

--- todo/settings.py
+++ todo/settings.py
@@ -126,3 +126,16 @@ LOGIN_URL = '/login/'
 
 LOGIN_REDIRECT_URL = '/todo_list/'
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
